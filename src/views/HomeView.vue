e89c97b00f5ec754e587af0b6e1f6097ea129b31
Xavi Lee <awxiaoxian2020@163.com>
2022-07-25 09:53:29 +0800

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <button type="button" class="btn btn-primary">Success</button>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
