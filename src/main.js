c656811b3a86ae5539a16664d6a77d483fa8dc83
Xavi Lee <awxiaoxian2020@163.com>
2022-07-26 13:42:50 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import router from './router'
 import store from './store'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap"
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app'
